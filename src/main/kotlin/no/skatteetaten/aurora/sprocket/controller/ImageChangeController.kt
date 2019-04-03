b16d3c55dc9295560de800d3f4c7efb9931ce41e
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2019-04-03 09:43:36 +0200

    endpoints for receiving events from docker

---
+++ src/main/kotlin/no/skatteetaten/aurora/sprocket/controller/ImageChangeController.kt
@@ -0,0 +1,29 @@
+package no.skatteetaten.aurora.sprocket.controller
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val logger = KotlinLogging.logger{}
+
+@RestController
+class ImageChangeController {
+
+    @RequestMapping("/global")
+    fun logGlobalEvent(@RequestBody eventPayload: JsonNode) {
+        logger.info {
+            jacksonObjectMapper().writeValueAsString(eventPayload)
+        }
+    }
+
+    @RequestMapping("/repogup")
+    fun logRepositoryEvent(@RequestBody eventPayload: JsonNode) {
+        logger.info {
+            jacksonObjectMapper().writeValueAsString(eventPayload)
+        }
+    }
+
+}
