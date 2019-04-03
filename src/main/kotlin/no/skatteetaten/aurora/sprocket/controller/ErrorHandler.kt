1d29ee24504e265e4ef6f394658dd78d7e221184
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2019-04-03 09:27:06 +0200

    removed unused exception and handling

--- src/main/kotlin/no/skatteetaten/aurora/sprocket/controller/ErrorHandler.kt
+++ src/main/kotlin/no/skatteetaten/aurora/sprocket/controller/ErrorHandler.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-private val logger = KotlinLogging.logger {}
+private val errorLogger = KotlinLogging.logger {}
 
 @ControllerAdvice
 class ErrorHandler : ResponseEntityExceptionHandler() {
@@ -20,16 +20,9 @@ class ErrorHandler : ResponseEntityExceptionHandler() {
         return handleException(e, request, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
-    @ExceptionHandler(NoSuchResourceException::class)
-    fun handleResourceNotFound(e: NoSuchResourceException, request: WebRequest): ResponseEntity<Any>? {
-        return handleException(e, request, HttpStatus.NOT_FOUND)
-    }
-
     private fun handleException(e: Exception, request: WebRequest, httpStatus: HttpStatus): ResponseEntity<Any>? {
         val headers = HttpHeaders().apply { contentType = MediaType.APPLICATION_JSON }
-        logger.debug("Handle exception", e)
+        errorLogger.debug("Handle exception", e)
         return handleExceptionInternal(e, e, headers, httpStatus, request)
     }
 }
-
-class NoSuchResourceException(message: String) : RuntimeException(message)
