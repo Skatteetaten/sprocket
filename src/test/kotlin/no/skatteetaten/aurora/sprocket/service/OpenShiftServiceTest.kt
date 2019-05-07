e07edc160deb884149bc3167379ff419e9785d3f
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-05-07 10:53:51 +0200

    use setJsonFileAsBody on MockResponse (#5)

--- src/test/kotlin/no/skatteetaten/aurora/sprocket/service/OpenShiftServiceTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/sprocket/service/OpenShiftServiceTest.kt
@@ -7,8 +7,10 @@ import io.fabric8.kubernetes.api.model.RootPaths
 import io.fabric8.openshift.api.model.ImageStreamList
 import io.fabric8.openshift.client.DefaultOpenShiftClient
 import no.skatteetaten.aurora.mockmvc.extensions.mockwebserver.execute
+import no.skatteetaten.aurora.mockmvc.extensions.mockwebserver.setJsonFileAsBody
 import no.skatteetaten.aurora.sprocket.models.ImageChangeEvent
 import no.skatteetaten.aurora.sprocket.utils.ResourceLoader
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.Test
 
@@ -19,13 +21,12 @@ class OpenShiftServiceTest : ResourceLoader() {
     private var mockClient = DefaultOpenShiftClient(rootUrl)
     private val root = RootPaths()
     private val imageStreamList: ImageStreamList = loadJsonResource("imagestreams.json", "openshift")
-    private val imageStream= imageStreamList.items.first()
+    private val imageStream = imageStreamList.items.first()
     private val service = OpenShiftService(mockClient)
     private val event = ImageChangeEvent("no_skatteetaten_aurora", "test", "test.container.registry:443")
 
     @Test
     fun `test fetching imageStream for event`() {
-
 
         val requests = server.execute(root, imageStreamList) {
             val isList = service.findAffectedImageStreamResource(event)
@@ -39,16 +40,15 @@ class OpenShiftServiceTest : ResourceLoader() {
 
     @Test
     fun `test importing image`() {
-
-        val import= loadBufferResource("import.json", "openshift")
-        val requests= server.execute(import) {
-            val response= service.importImage(event, imageStream)
+        val response = MockResponse().setJsonFileAsBody("openshift/import.json")
+        val requests = server.execute(response) {
+            val response = service.importImage(event, imageStream)
             assertThat(response).isNotNull()
         }
         assertThat(requests.size).isEqualTo(1)
 
         val postRequest = requests[0]
-        assertThat(postRequest.body).isEqualTo(import)
+        assertThat(postRequest.body).isEqualTo(response.body)
         assertThat(postRequest.requestUrl.toString()).isEqualTo("${rootUrl}apis/image.openshift.io/v1/namespaces/aurora-test/imagestreamimports")
     }
 }
