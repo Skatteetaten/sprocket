28972ced82c1928cbecb153c05bdd6b370dbfac1
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-07 13:40:03 +0200

    gradlewrapper 5.5.1. version bump. sonarqube true in jenkinsfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,26 @@
+buildscript {
+    dependencies {
+        // must specify this in gradle.properties since the same version must be here and in aurora plugin
+        val springCloudContractVersion: String = project.property("aurora.springCloudContractVersion") as String
+        classpath("org.springframework.cloud:spring-cloud-contract-gradle-plugin:$springCloudContractVersion")
+    }
+}
+
 plugins {
     id("org.springframework.cloud.contract")
     id("org.jetbrains.kotlin.jvm") version "1.3.31"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
     id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+    id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.4.RELEASE"
+    id("org.springframework.boot") version "2.1.7.RELEASE"
     id("org.asciidoctor.convert") version "2.2.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
-    id("no.skatteetaten.gradle.aurora") version "2.3.1"
+    id("no.skatteetaten.gradle.aurora") version "2.4.0"
 }
 
 dependencies {
